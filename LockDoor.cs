38d26414b9b677d125f34aaf2d187838b2608ce6
Stanislas Castaybert <perso@stan1712.com>
2022-02-18 21:47:38 +0100

    ✨ feature・Ajout de la récupération des portes et calcul de la vélocité du véhicule

--- LockDoor.cs
+++ LockDoor.cs
@@ -5,4 +5,14 @@ public Program()
 
 public void Main()
 {
+	/** GET LOCKABLE DOORS **/
+	var DoorsList = new List<IMyTerminalBlock>();						
+	GridTerminalSystem.GetBlocksOfType<IMyDoor>(DoorsList);
+	Echo(DoorsList.Count.ToString());
+
+	/** GET VELOCITY **/
+    var cockpit_seat = GridTerminalSystem.GetBlockWithName("Contrôle Principal") as IMyCockpit;
+    var velocity = cockpit_seat.GetShipVelocities().LinearVelocity.Sum;
+
+    Echo("Vélocité : " + velocity.ToString());
 }
